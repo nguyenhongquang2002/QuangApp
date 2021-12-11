8e4cc69a9715947d30c87058535c601702b8c5a5
nguyenhongquang2002 <71063187+nguyenhongquang2002@users.noreply.github.com>
2021-12-11 11:44:35 +0700

    Add project files.

---
+++ QuangApp/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using QuangApp.Data;
+using QuangApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuangApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MyDBContext myDBContext = new MyDBContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Apple()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Store(string name )
+        {
+            var student = new Student()
+            {
+                Name = name               
+            };
+            myDBContext.Students.Add(student);
+            myDBContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ QuangApp/Data/MyDBContext.cs
@@ -0,0 +1,18 @@
+﻿using QuangApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace QuangApp.Data
+{
+    public class MyDBContext: DbContext
+    {
+        public MyDBContext():base("name = MyDBContext")
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
